c05bb0aa3a5aea8d38ab9ec606db09db1f4a2e54
MvP Mark <mvp.paulo@gmail.com>
2020-01-18 03:30:57 -0400

    4rd commit

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,StatusBar} from 'react-native';
+
+import Routes from './src/routes';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Waltinho</Text>
-    </View>
-  );
-}
+    <>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+    <Routes/>
+    </>);
+  }
+ 
